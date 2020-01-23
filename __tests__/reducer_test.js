168f9a8639254957ffaed00c40682423113ccaf2
Mark Newcomb <wlmmn@hotmail.com>
2020-01-23 00:09:23 +0000

    testing reducers

---
+++ __tests__/reducer_test.js
@@ -0,0 +1,18 @@
+// ./**tests**/reducer_test.js
+import reducer from '../src/redux/reducer'
+import {initialState} from '../src/redux/reducer'
+
+describe('todos reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toMatchSnapshot()
+    })
+
+    it('should handle TODO_REQUEST', () => {
+        expect(
+            reducer(initialState,
+                {
+                    type: 'TODO_REQUEST'
+                })
+        ).toMatchSnapshot()
+    })
+})
--- __tests__/todo_component_test.js
+++ __tests__/todo_component_test.js
@@ -1,36 +1,48 @@
 // ./**tests**/todo_component_test.js
-describe('Todo form', () => {
+describe('Editing todos', () => {
     let wrapper, props_;
+    //In this before each, we are opening the form and changing the to-do title value before each of the tests is run. This helps us to avoid having to do this repeatedly for every it block.
     beforeEach(() => {
-        // spy on the component handleOpen method
-        sinon.spy(Todo.prototype, "handleOpen");
+        // spy on the component handleFieldChange method
+        sinon.spy(Todo.prototype, "handleFieldChange");
+        // spy on the component handleEdit method
+        sinon.spy(Todo.prototype, "handleEdit");
+        // spy on the component handleClose method
+        sinon.spy(Todo.prototype, "handleClose");
         const { enzymeWrapper, props } = shallowSetup();
         wrapper = enzymeWrapper;
         props_ = props;
+        const button = wrapper.find('button').first();
+        button.simulate('click');
+        // find the input field containing the todo title and simulate a change to it's value
+        const titleInput = wrapper.find('input').at(0);
+        titleInput.simulate('change', {
+            target: {
+                value: 'Changed title',
+                name: 'title'
+            },
+        });
     });
     afterEach(() => {
-        Todo.prototype.handleOpen.restore();
+        Todo.prototype.handleFieldChange.restore();
+        Todo.prototype.handleEdit.restore();
+        Todo.prototype.handleClose.restore();
     });
-    it('should update the state property _**`formOpen`**_ and call handleOpen when edit button is clicked', () => {
-        // find the edit button and simulate a click on it
-        const button = wrapper.find('button').first();
-        button.simulate('click');
-        // The handleOpen method should be called.
-        expect(Todo.prototype.handleOpen.calledOnce).toBe(true);
-        // The value of this.state.formOpen should now be true
-        expect(wrapper.state().formOpen).toEqual(true);
+    it('should change state when input values change and call handleFieldChange', () => {
+        // this.state.todo should now have a title field with it's value as the new title we entered.
+        expect(wrapper.state().todo.title).toEqual('Changed title');
+        // Since we simulated a change to an input field, the handleFieldChange event handler should be called.
+        expect(Todo.prototype.handleFieldChange.calledOnce).toBe(true);
     });
-    it('should display different buttons', () => {
-        const button = wrapper.find('button').first();
-        button.simulate('click');
-        // When we click the edit button, the Update button should be present.
-        expect(wrapper.find('button.ui').length).toBe(2);
-        expect(wrapper.find('button.ui.basic.green.button').text()).toBe(' Update');
-    });
-    it('should display current values in edit fields', () =>{
-        const button = wrapper.find('button').first();
-        button.simulate('click');
-        // Before any edits are made, the prepopulated values in the input fields should be the same passed through props.
-        expect(wrapper.find('input').at(0).props().defaultValue).toEqual(props_.title);
+    describe('Submit edits', () => {
+        it('should call handleEdit, editTodo and handleClose when update button is clicked', () => {
+            const button = wrapper.find('button.ui.basic.green.button');
+            button.simulate('click');
+            // Confirm that the different component methods called when we submit edits are called.
+            expect(Todo.prototype.handleEdit.calledOnce).toBe(true);
+            expect(Todo.prototype.handleClose.calledOnce).toBe(true);
+            // the mock function we passed to the renderer instead of the action should be called and with the new values we entered.
+            expect(editTodo).toBeCalledWith(props_.id, {"title": "Changed title"});
+        });
     });
 });
